523f8535f1179226acd174b2efe7ae1e43b811d9
marta514 <mglmonsivais255@gmail.com>
2025-08-10 05:14:31 -0600

    Version Final

---
+++ women game jam/women-game-jam/objeto_oculto.gd
@@ -0,0 +1,34 @@
+class_name ObjetoRO
+extends Area2D
+
+# Define una señal que será emitida cuando el objeto sea recolectado
+signal recolectado
+
+var can_interact = false
+var body_in_area = null
+
+@export var dialogo_texto: String = "¡Órale! , esto si que es un objeto interesante"
+
+# Llamada cuando un cuerpo entra en el Area2D
+func _on_body_entered(body):
+	if body.is_in_group("Personaje"):
+		can_interact = true
+		body_in_area = body
+		print("Puedes interactuar con el objeto.")
+
+# Llamada cuando un cuerpo sale del Area2D
+func _on_body_exited(body):
+	if body_in_area == body:
+		can_interact = false
+		body_in_area = null
+		print("Ya no puedes interactuar con el objeto.")
+
+# Llama a _input() para manejar la entrada
+func _input(event):
+	if can_interact and event.is_action_pressed("recolectar"):
+		if body_in_area != null:
+			# Emite la señal antes de eliminar el objeto
+			emit_signal("recolectado", dialogo_texto)
+			# Elimina el objeto solo si el jugador presiona el botón de interacción
+			queue_free()
+			print("Objeto recolectado!")
--- women game jam/women-game-jam/scene/player.gd
+++ women game jam/women-game-jam/scene/player.gd
@@ -1,9 +1,44 @@
+class_name Personaje
 extends CharacterBody2D
 
-@export var move_speed = 125
+signal touch_obj()
+const SPEED = 100.0
+var inventario: Array[int] = []
 
+@onready var animacion = $Sprite2D 
 
-func _physics_process(delta):
-	var input_axis = Input.get_axis("mov-izquierda","mov-derecha")
-	velocity.x = input_axis * move_speed
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction_x = Input.get_axis("izquierda", "derecha")
+	var direction_y = Input.get_axis("arriba", "abajo")
+	
+	var direction = Vector2(direction_x, direction_y).normalized()
+
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.y = direction.y * SPEED
+
+		# Controla las animaciones
+		if direction.x > 0:
+			animacion.play("derecha")
+		elif direction.x < 0:
+			animacion.play("izquierda")
+		elif direction.y > 0:
+			animacion.play("abajo")
+		elif direction.y < 0:
+			animacion.play("arriba")
+	else:
+		# Si el personaje no se mueve, reproduce la animación "idle"
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		animacion.play("idle")
+		
 	move_and_slide()
+
+
+func recolectar():
+	print("Recogido")
